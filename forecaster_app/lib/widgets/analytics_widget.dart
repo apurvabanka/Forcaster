151beb05b9b6181a3fbb1da3c5b96186815ac046
Raj-Kumar-Alikatte <rajalikatte@gmail.com>
2024-12-04 23:38:36 +0530

    com

--- forecaster_app/lib/widgets/analytics_widget.dart
+++ forecaster_app/lib/widgets/analytics_widget.dart
@@ -2,23 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:forecaster_app/widgets/average_rating_widget.dart';
 import 'package:forecaster_app/widgets/cluster_widget.dart';
 import 'package:forecaster_app/widgets/headerWidget.dart';
+import 'package:forecaster_app/widgets/vehicles_widget.dart';
 
 class AnalyticsWidget extends StatelessWidget {
   const AnalyticsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 18),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
       child: Column(
         children: [
-          SizedBox(height: 18,),
-          Headerwidget(),
-          SizedBox(height: 18,),
-          AverageRatingWidget(),
-          SizedBox(height: 18,),
-          ClusterWidget(),
-          SizedBox(height: 200,),
+          const SizedBox(height: 18,),
+          const Headerwidget(),
+          const SizedBox(height: 18,),
+          const AverageRatingWidget(),
+          const SizedBox(height: 18,),
+          const ClusterWidget(),
+          const SizedBox(height: 18,),
+          VehicleEfficiencyApp(
+            
+          )
         ],
       ),
     );
