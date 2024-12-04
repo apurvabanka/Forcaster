462e6ffd34bbee6ff824461071bd3dda79c05e85
apurvabanka <appu.banka@gmail.com>
2024-12-04 05:12:04 -0500

    Delivery Backend

--- forecaster_backend/delivery/admin.py
+++ forecaster_backend/delivery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
-from delivery.models import DeliveryRecord
+from delivery.models import DeliveryRecord, RegressionResult
 
 admin.site.register(DeliveryRecord, ImportExportActionModelAdmin)
+admin.site.register(RegressionResult)
