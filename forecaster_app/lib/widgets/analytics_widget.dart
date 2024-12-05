3ccbd97afb54ad7960c42be6f3fd8446b74943a8
apurvabanka <appu.banka@gmail.com>
2024-12-05 00:23:38 -0500

    Fixed height of scroll in Analytics

--- forecaster_app/lib/widgets/analytics_widget.dart
+++ forecaster_app/lib/widgets/analytics_widget.dart
@@ -12,21 +12,26 @@ class AnalyticsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        children: [
-          const SizedBox(height: 18,),
-          const Headerwidget(),
-          const SizedBox(height: 18,),
-          const AverageRatingWidget(),
-          const SizedBox(height: 18,),
-          const ClusterWidget(),
-          const SizedBox(height: 18,),
-          VehicleEfficiencyApp(),
-          const SizedBox(height: 18,),
-          //WeatherImpactDelivery()
-        ],
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        physics: BouncingScrollPhysics(),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 1.50,
+          child: Column(
+            children: [
+              const SizedBox(height: 18,),
+              const AverageRatingWidget(),
+              const SizedBox(height: 18,),
+              const ClusterWidget(),
+              const SizedBox(height: 18,),
+              VehicleEfficiencyApp(),
+              const SizedBox(height: 18,),
+              WeatherImpactDelivery(),
+              const SizedBox(height: 18,),
+            ],
+          ),
+        ),
       ),
-
     );
   }
 }
