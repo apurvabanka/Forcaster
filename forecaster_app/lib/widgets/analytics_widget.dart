6e7744879d52fee8b62f27839c46e79c91f2a731
Apurva Banka <31559151+apurvabanka@users.noreply.github.com>
2024-12-07 05:14:03 -0500

    Harshitha hypo (#42)
    * crash
    
    * cmt
    
    * Fixed multple delivery graph
    
    * added data
    
    * cmd
    
    ---------
    
    Co-authored-by: Raj-Kumar-Alikatte <rajalikatte@gmail.com>

--- forecaster_app/lib/widgets/analytics_widget.dart
+++ forecaster_app/lib/widgets/analytics_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:forecaster_app/widgets/area_ratings.dart';
 import 'package:forecaster_app/widgets/average_rating_widget.dart';
 import 'package:forecaster_app/widgets/cluster_widget.dart';
+import 'package:forecaster_app/widgets/multiple_deliveries.dart';
 import 'package:forecaster_app/widgets/vehicles_widget.dart';
 import 'package:forecaster_app/widgets/weather_conditions.dart';
 
@@ -15,7 +17,7 @@ class AnalyticsWidget extends StatelessWidget {
         scrollDirection: Axis.vertical,
         physics: const BouncingScrollPhysics(),
         child: SizedBox(
-          height: MediaQuery.of(context).size.height * 2.0,
+          height: MediaQuery.of(context).size.height * 4.0,
           child: Column(
             children: [
               const SizedBox(height: 18,),
@@ -26,6 +28,10 @@ class AnalyticsWidget extends StatelessWidget {
               VehicleEfficiencyApp(),
               const SizedBox(height: 18,),
               WeatherImpactDelivery(),
+              const SizedBox(height: 18,),
+              TimeBasedDeliveries(),
+              const SizedBox(height: 18,),
+              const AreaRatingsComparison(),
               const SizedBox(height: 18,),
             ],
           ),
